e44a511a46c4ca28531b319e3bf19d1dd2fdea89
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-10-24 19:53:17 +0500

    start

---
+++ src/Amaliyot.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Amaliyot.scss'
+function Amaliyot() {
+  return (
+    <div className='Amaliyot'>
+    <div class="container">
+        <div class="card">
+            <div class="content">
+                <h2>01</h2>
+                <h3>Card One</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Facilis dolore quam exercitationem nobis blanditiis totam,
+                     sint nam autem neque porro ratione.</p>
+                     <a href="#">Read More</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                <h2>02</h2>
+                <h3>Card Two</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Facilis dolore quam exercitationem nobis blanditiis totam,
+                     sint nam autem neque porro ratione.</p>
+                     <a href="#">Read More</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                <h2>03</h2>
+                <h3>Card Three</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Facilis dolore quam exercitationem nobis blanditiis totam,
+                     sint nam autem neque porro ratione.</p>
+                     <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Amaliyot
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Amaliyot from './Amaliyot';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Amaliyot/>
+    </div>
+  );
+}
+
+export default App;
